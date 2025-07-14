155ff3b8414840a7c8dc93e4a091e18b3283110e
eric <eric@polysemicservices.com>
2025-07-14 08:51:47 -0700

    Navigatioin fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,120 +1,126 @@
-jQuery(function ($) {
+$(document).ready(function() {
     'use strict';
-    /*----------- Preloader -----------*/
-    // jQuery(window).load(function () {
-    //   jQuery('.loadingFade').delay(2500).fadeOut('slow');
-    //   jQuery('.pinFade1').delay(2500).fadeOut('slow');
-    //   jQuery('.pinFade2').delay(2500).fadeOut('slow');
-    //   jQuery('.pinFade3').delay(2500).fadeOut('slow');
-    //   jQuery('.preloader').delay(2500).fadeOut('slow');
-    // });
-    /*
-    $(window).resize(function () {
-        $(".navbar-collapse").css({maxHeight: $(window).height() - $(".navbar-header").height() + "px"});
+    
+    // Initialize preloader if DEPreLoad is available
+    if (typeof $.fn.DEPreLoad !== 'undefined') {
+        $("body").DEPreLoad();
+    }
+    
+    // Mobile navigation toggle functionality
+    $('.navbar-toggle').on('click', function() {
+        var target = $(this).attr('data-target');
+        $(target).toggleClass('in');
+        $(this).toggleClass('collapsed');
+        
+        // Toggle aria-expanded attribute for accessibility
+        var expanded = $(this).attr('aria-expanded') === 'true';
+        $(this).attr('aria-expanded', !expanded);
     });
-    //sticky navbar
-    $(".navbar-sticky").sticky({topSpacing: 0});
-    //menu on hover
-    $('.js-activated').dropdownHover({
-        instantlyCloseOthers: false,
-        delay: 0
-    }).dropdown();
-
-    $('.counter-digit').counterUp({
-        delay: 10,
-        time: 5000
-    });
-   
-    $(".dropdown").hover(            
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-            $(this).toggleClass('open');        
-        },
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-            $(this).toggleClass('open');       
+    
+    // Close mobile menu when clicking on a link
+    $('.navbar-nav a').on('click', function() {
+        if ($(window).width() < 768) {
+            $('.navbar-collapse').removeClass('in');
+            $('.navbar-toggle').addClass('collapsed').attr('aria-expanded', 'false');
         }
-    );
-    $('.menubtn').collapse();
-});
-*/ 
-    // $(document).on('click', '.dropdown',            
-        // function() {
-        //     $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-        //     $(this).toggleClass('open');        
-        // },
-        // function() {
-        //     $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-        //     $(this).toggleClass('open');       
-        // }
-    // );
-    /*
-    $(document).on('click', '.dropdown a',function() {
-        if($(this).parent().hasClass('open')){
-            $(this).next().hide().end().parent().siblings().find('.mega-dropdown-menu').slideUp("400");
-            $(this).next().stop(true,true).slideDown("400");
-        }else{
-            $(this).next().stop(true,true).slideUp("400");
-        }            
     });
-    $(document).mouseup(function (e){
-    var container = $('.mega-dropdown, .mega-dropdown-menu');
-
-    if (!container.is(e.target) && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        $('.mega-dropdown-menu').slideUp("400");
+    
+    // Handle dropdown functionality for mobile
+    $('.dropdown-toggle').on('click', function(e) {
+        if ($(window).width() < 768) {
+            e.preventDefault();
+            var $dropdown = $(this).parent();
+            var $menu = $dropdown.find('.dropdown-menu');
+            
+            // Close other open dropdowns
+            $('.dropdown').not($dropdown).removeClass('open');
+            $('.dropdown-menu').not($menu).slideUp();
+            
+            // Toggle current dropdown
+            $dropdown.toggleClass('open');
+            $menu.slideToggle();
+        }
+    });
+    
+    // Close dropdowns when clicking outside
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.dropdown').length) {
+            $('.dropdown').removeClass('open');
+            $('.dropdown-menu').slideUp();
+        }
+    });
+    
+    // Handle window resize
+    $(window).resize(function() {
+        if ($(window).width() >= 768) {
+            // Reset mobile menu state on desktop
+            $('.navbar-collapse').removeClass('in');
+            $('.navbar-toggle').addClass('collapsed').attr('aria-expanded', 'false');
+            $('.dropdown').removeClass('open');
+            $('.dropdown-menu').removeAttr('style');
+        }
+    });
+    
+    // Initialize Bootstrap components if available
+    if (typeof $.fn.dropdown !== 'undefined') {
+        $('.dropdown-toggle').dropdown();
+    }
+    
+    // Initialize revolution slider if it exists
+    if (typeof $.fn.revolution !== 'undefined' && $('.tp-banner').length) {
+        $('.tp-banner').revolution({
+            delay: 9000,
+            startwidth: 1170,
+            startheight: 500,
+            hideThumbs: 200,
+            thumbWidth: 100,
+            thumbHeight: 50,
+            thumbAmount: 5,
+            navigationType: "bullet",
+            navigationArrows: "solo",
+            navigationStyle: "preview1",
+            touchenabled: "on",
+            onHoverStop: "on",
+            swipe_velocity: 0.7,
+            swipe_min_touches: 1,
+            swipe_max_touches: 1,
+            drag_block_vertical: false,
+            parallax: "mouse",
+            parallax_levels: [7,4,3,2,5,4,3,2,1,0],
+            keyboardNavigation: "off",
+            navigationHAlign: "center",
+            navigationVAlign: "bottom",
+            navigationHOffset: 0,
+            navigationVOffset: 20,
+            soloArrowLeftHalign: "left",
+            soloArrowLeftValign: "center",
+            soloArrowLeftHOffset: 20,
+            soloArrowLeftVOffset: 0,
+            soloArrowRightHalign: "right",
+            soloArrowRightValign: "center",
+            soloArrowRightHOffset: 20,
+            soloArrowRightVOffset: 0,
+            shadow: 0,
+            fullWidth: "on",
+            fullScreen: "off",
+            spinner: "spinner4",
+            stopLoop: "off",
+            stopAfterLoops: -1,
+            stopAtSlide: -1,
+            shuffle: "off",
+            autoHeight: "off",
+            forceFullWidth: "off",
+            hideThumbsOnMobile: "off",
+            hideNavDelayOnMobile: 1500,
+            hideBulletsOnMobile: "off",
+            hideArrowsOnMobile: "off",
+            hideThumbsUnderResolution: 0,
+            hideSliderAtLimit: 0,
+            hideCaptionAtLimit: 0,
+            hideAllCaptionAtLilmit: 0,
+            startWithSlide: 0,
+            videoJsPath: "rs-plugin/videojs/",
+            fullScreenOffsetContainer: ""
+        });
     }
 });
-    $('.menubtn').collapse();
-    $('.navbar-toggle').on('click', function () {
-       var target = $(this).data('apply');
-       $('#'+target).slideToggle();
-    });
-    */
-    /*
-    $(document).ready(function() {
-        var loader = $("body").DEPreLoad({
-            OnStep: function(percent) {
-                // console.log(percent);
-                // console.log();
-                var getFigure = Math.floor(Math.random() * ((80-40)+1) + 40);
-                var getFigure2 = Math.floor(Math.random() * ((90-10)+1) + 20);
-                console.log(Math.round(percent));
-                if(Math.round(percent/10) <= 10){
-                    $('.pinFade'+Math.round(percent/10)).addClass('animate').css({
-                        top: Math.floor((Math.random() * 80) + 20)+'%',
-                        left: Math.floor((Math.random() * 80) + 20)+'%',
-                        width: getFigure + 'px',
-                        backgroundSize: 'contain',
-                        animationDelay: ((Math.round(percent/10))*0.1)+'s',
-                    });
-                }
-                if(Math.round(percent/10) == 10){
-                    $('.pinFade11').addClass('animate').css({
-                        top: Math.floor((Math.random() * 80) + 20)+'%',
-                        left: Math.floor((Math.random() * 80) + 20)+'%',
-                        width: getFigure + 'px',
-                        backgroundSize: 'contain',
-                        animationDelay: 2.2+'s',
-                    });
-                    $('.pinFade12').addClass('animate').css({
-                        top: Math.floor((Math.random() * 80) + 20)+'%',
-                        left: Math.floor((Math.random() * 80) + 20)+'%',
-                        width: getFigure + 'px',
-                        backgroundSize: 'contain',
-                        animationDelay: 2.4+'s',
-                    });
-                }
-                // console.log('.pinFade'+percent/10);
-            },
-            OnComplete: function() {
-                setTimeout(function () {
-                   $('.preloader').fadeOut();
-                }, 1500);
-            }
-        });
-    });
-    */
-});
-// All menu toggle JS is now handled by Bootstrap. No custom nav JS needed.
-
